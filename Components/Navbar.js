099c85d69d24654151dedb6b3493f49a4bd5a0f5
Pizatol <dev.belmonte@gmail.com>
2022-03-13 19:34:07 +0100

    creation navbar

---
+++ Components/Navbar.js
@@ -0,0 +1,38 @@
+import css from "../styles/NavBar.module.css";
+
+import React from "react";
+
+export default function Navbar() {
+    return (
+        <div className={css.NavbarContainer}>
+
+
+
+            <div className={css.buttonContainer}>
+
+
+                <button className={css.button}>Home</button>
+
+                <div className={css.projects}>
+                    <button className={css.button}>Projects</button>
+                    <ul>
+                        <li>Link</li>
+                        <li>Link</li>
+                        <li>Link</li>
+                        <li>Link</li>
+                    </ul>
+                </div>
+
+                <div className={css.products}>
+                    <button className={css.button}>Products</button>
+                    <ul>
+                        <li>Link</li>
+                        <li>Link</li>
+                        <li>Link</li>
+                        <li>Link</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    );
+}
